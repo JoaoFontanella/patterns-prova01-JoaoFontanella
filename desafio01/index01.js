a65e05a37b7d9dad5a20101aca0e400f11b71af9
JoaoFontanella <joaofontanella31@gmail.com>
2025-09-18 19:59:14 -0300

    Prova feita

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,74 @@
-console.log("Hello, World! 001");
+class Lanche {
+    constructor() {
+        this.pao = null;
+        this.carne = null;
+        this.queijo = null;
+        this.salada = null;
+        this.molho = null;
+    }
+
+    show() {
+        console.log("Lanche:", {
+            pao: this.pao,
+            carne: this.carne,
+            queijo: this.queijo,
+            salada: this.salada,
+            molho: this.molho,
+        });
+    }
+}
+
+// Builder
+class LancheBuilder {
+    constructor() {
+        this.lanche = new Lanche();
+    }
+
+    setPao(pao) {
+        this.lanche.pao = pao;
+        return this;
+    }
+
+    setCarne(carne) {
+        this.lanche.carne = carne;
+        return this;
+    }
+
+    setQueijo(queijo) {
+        this.lanche.queijo = queijo;
+        return this;
+    }
+
+    setSalada(salada) {
+        this.lanche.salada = salada;
+        return this;
+    }
+
+    setMolho(molho) {
+        this.lanche.molho = molho;
+        return this;
+    }
+
+    build() {
+        return this.lanche;
+    }
+}
+
+// Director → Pao pré-definido
+class LancheDirector {
+    static buildLancheTOP() {
+      return new LancheBuilder()
+        .setPao("Pão Ceverjinho")
+        .setCarne("Churrasco")
+        .setQueijo("Queijo prato")
+        .setSalada("Tomate")
+        .setMolho("BBQ")
+        .build();
+    }
+}
+
+const lanchepredefinido = LancheDirector.buildLancheTOP();
+lanchepredefinido.show();
+
+const lanchecustom = new LancheBuilder().setPao("Pao").setCarne("Frango").setQueijo("Chedar").setSalada("Alface").setMolho("Mostarda").build();
+lanchecustom.show();
--- desafio01/index02.js
+++ desafio01/index02.js
@@ -1,1 +1,26 @@
-console.log("Hello, World! 002");
+class Config {
+  constructor() {
+    if (Config.instance) {
+      return Config.instance; 
+    }
+
+    this.settings = {};
+    Config.instance = this; 
+  }
+
+  setConfig(key, value) {
+    this.settings[key] = value;
+  }
+
+  getConfig(key) {
+    return this.settings[key];
+  }
+}
+
+const lang01 = new Config();
+lang01.setConfig("lang01", "pt-BR");
+const lang02 = new Config();
+lang02.setConfig("lang02", "eu-US")
+const lang03 = new Config();
+console.log(lang03.getConfig("lang01"));
+console.log(lang03.getConfig("lang02"));
